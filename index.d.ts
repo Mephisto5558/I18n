d9967299109753cb0f1f4b0872f1f398d4b68d8d
Mephisto5558 <87113906+Mephisto5558@users.noreply.github.com>
2024-02-09 17:10:22 +0100

    typing stuff

--- index.d.ts
+++ index.d.ts
@@ -1,30 +1,31 @@
 export = I18nProvider;
 declare class I18nProvider {
   constructor(
-    localesPath?:string, defaultLocale?:string, separator?:string,
-    notFoundMessage?:string?, errorNotFound?:boolean, undefinedNotFound?:boolean
-  ): I18nProvider
+    localesPath?: string, defaultLocale?: string, separator?: string,
+    notFoundMessage?: string, errorNotFound?: boolean, undefinedNotFound?: boolean
+  );
 
   config: {
-    localesPath: string, defaultLocale: string, separator: string,
-    errorNotFound: boolean, undefinedNotFound: boolean, notFoundMessage: string
-  }
+    localesPath: string; defaultLocale: string; separator: string;
+    errorNotFound: boolean; undefinedNotFound: boolean; notFoundMessage: string;
+  };
+
   /**<locale key, file path>*/
-  availableLocales: Map<string, string>
-  localeData: object
+  availableLocales: Map<string, string>;
+  localeData: object;
 
-  loadLocale(locale:string):Promise<void>
-  loadAllLocales():Promise<void>
+  loadLocale(locale: string): Promise<void>;
+  loadAllLocales(): Promise<void>;
 
-  /**@returns the message*/
+  /** @returns the message*/
   __(
-    config?: {locale?:string, errorNotFound?:boolean, undefinedNotFound?:boolean, backupPath?:string},
+    config: { locale?: string; errorNotFound?: boolean; undefinedNotFound?: boolean; backupPath?: string },
     key: string, replacements?: string|object
-    ): string
+  ): string;
 
-  /**@return flatted object*/
-  flatten(object:object, objectPath:string):object
+  /** @returns flatted object*/
+  flatten(object: object, objectPath: string): object;
 
-  /**@returns list of entries that are missing or equal with default data*/
-  findMissing(checkEqual:boolean): object
+  /** @returns list of entries that are missing or equal with default data*/
+  findMissing(checkEqual: boolean): object;
 }
