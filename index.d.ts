74b7251515a952f58a81c4345ec590aceeaa2839
Mephisto5558 <87113906+Mephisto5558@users.noreply.github.com>
2024-03-02 10:08:11 +0100

    switched to using a paramete for the warning log function

--- index.d.ts
+++ index.d.ts
@@ -2,7 +2,8 @@ export = I18nProvider;
 declare class I18nProvider {
   constructor(
     localesPath?: string, defaultLocale?: string, separator?: string,
-    notFoundMessage?: string, errorNotFound?: boolean, undefinedNotFound?: boolean
+    notFoundMessage?: string, errorNotFound?: boolean, undefinedNotFound?: boolean,
+    warnLoggingFunction?: (...msg: string) => unknown
   );
 
   config: {
@@ -28,4 +29,6 @@ declare class I18nProvider {
 
   /** @returns list of entries that are missing or equal with default data*/
   findMissing(checkEqual: boolean): object;
+
+  logWarn(...msg: string): unknown;
 }
